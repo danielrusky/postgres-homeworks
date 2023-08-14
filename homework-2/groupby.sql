9af65bcda8e8f5483ce149938524e4379c9f562f
danielrusky <52462451+danielrusky@users.noreply.github.com>
2023-08-14 17:31:36 +0300

    Update groupby.sql

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -1,20 +1,25 @@
 -- Напишите запросы, которые выводят следующую информацию:
--- 1. заказы, отправленные в города, заканчивающиеся на 'burg'. Вывести без повторений две колонки (город, страна) (см. таблица orders, колонки ship_city, ship_country)
+-- 1. заказы, доставленные в страны France, Germany, Spain (таблица orders, колонка ship_country)
+SELECT *
+FROM orders
+WHERE ship_country IN ('France', 'Germany', 'Spain');
 
+-- 2. уникальные города и страны, куда отправлялись заказы, отсортировать по странам и городам (таблица orders, колонки ship_country, ship_city)
+SELECT DISTINCT ship_city, ship_country
+FROM orders
+ORDER BY ship_city, ship_country;
 
--- 2. из таблицы orders идентификатор заказа, идентификатор заказчика, вес и страну отгрузки. Заказ отгружен в страны, начинающиеся на 'P'. Результат отсортирован по весу (по убыванию). Вывести первые 10 записей.
+-- 3. сколько дней в среднем уходит на доставку товара в Германию (таблица orders, колонки order_date, shipped_date, ship_country)
+SELECT AVG(shipped_date - order_date)
+FROM orders
+WHERE ship_country = 'Germany'
 
+-- 4. минимальную и максимальную цену среди продуктов, не снятых с продажи (таблица products, колонки unit_price, discontinued не равно 1)
+SELECT MIN(unit_price), MAX(unit_price)
+FROM products
+WHERE discontinued != 1
 
--- 3. фамилию, имя и телефон сотрудников, у которых в данных отсутствует регион (см таблицу employees)
-
-
--- 4. количество поставщиков (suppliers) в каждой из стран. Результат отсортировать по убыванию количества поставщиков в стране
-
-
--- 5. суммарный вес заказов (в которых известен регион) по странам, но вывести только те результаты, где суммарный вес на страну больше 2750. Отсортировать по убыванию суммарного веса (см таблицу orders, колонки ship_region, ship_country, freight)
-
-
--- 6. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers) и работники (employees).
-
-
--- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
+-- 5. минимальную и максимальную цену среди продуктов, не снятых с продажи и которых имеется не меньше 20 (таблица products, колонки unit_price, units_in_stock, discontinued не равно 1)
+SELECT MIN(unit_price), MAX(unit_price)
+FROM products
+WHERE discontinued <> 1 and units_in_stock >= 20;
